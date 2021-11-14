29c0d1e59f9b52d452739cf732b80cd9ccf905bb
shashivendra <shashivendrasingh75@gmail.com>
2021-11-14 11:42:58 +0530

    first update

---
+++ gitdummyProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitdummyProject
+//
+//  Created by shashivendra  on 14/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, swift ui update!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
